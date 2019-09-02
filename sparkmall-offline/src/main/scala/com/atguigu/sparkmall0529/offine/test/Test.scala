3d34ffedbf4f3b022299546e08bd59279076deb2
kelvin <734536227@qq.com>
2019-09-02 22:58:53 +0800

    hello world

---
+++ sparkmall-offline/src/main/scala/com/atguigu/sparkmall0529/offine/test/Test.scala
@@ -0,0 +1,11 @@
+package com.atguigu.sparkmall0529.offine.test
+
+/**
+  * Created by kelvin on 2019/9/2.
+  */
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
